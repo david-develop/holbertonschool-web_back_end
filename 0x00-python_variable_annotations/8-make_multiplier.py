632d2463baa9d13818980070024fee3a1dbc1029
david-develop <924@holbertonschool.com>
2021-07-19 23:06:24 -0500

    8f

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """ type-annotated function make_multiplier that takes a float multiplier as
 argument and returns a function that multiplies a float by multiplier."""
 
+
 from typing import Callable
 
 
