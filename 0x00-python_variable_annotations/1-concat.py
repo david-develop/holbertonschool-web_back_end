c998d8f0714c8838cc5b529c26fea952abcbe1fa
david-develop <924@holbertonschool.com>
2021-07-19 21:41:03 -0500

    concat str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a"""
+
+
+def concat(a: str, b: str) -> str:
+    """type-annotated function concat that takes a string str1 and
+    a string str2 as arguments and returns a
+    Args:
+        a: string
+        b: string
+    Returns:
+        string concat a + b
+    """
+    return a + b
