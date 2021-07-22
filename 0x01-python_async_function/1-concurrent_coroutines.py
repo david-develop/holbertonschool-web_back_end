12ff9c33a44a4a371e465b5d605a6f3166edac3b
david-develop <924@holbertonschool.com>
2021-07-21 21:25:31 -0500

    1f

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -17,7 +17,7 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     for _ in range(n):
         res = wait_random(max_delay)
         list_sec.append(res)
-    
+
     for q in asyncio.as_completed(list_sec):
         result = await q
         sorted_list.append(result)
