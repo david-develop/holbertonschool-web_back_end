0bb02a57912a4a2f2e7afbe623e59d6d780c45c3
david-develop <924@holbertonschool.com>
2021-07-21 19:39:15 -0500

    random delay

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Synchronous coroutine that takes in an integer argument that waits for a
+random delay  and eventually returns it."""
+
+
+from random import uniform
+import asyncio
+
+
+async def wait_random(max_delay: int = 10):
+    """Synchronous coroutine that takes in an integer argument that waits
+    for a random delay  and eventually returns it."""
+    ran_num = uniform(0, max_delay)
+    await asyncio.sleep(ran_num)
+    return ran_num
