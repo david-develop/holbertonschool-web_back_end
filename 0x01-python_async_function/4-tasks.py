f955979d29e5628d6a9d144a90b644d0b6eb9520
david-develop <924@holbertonschool.com>
2021-07-21 21:23:03 -0500

    task creation

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Function that takes an integer max_delay and returns a asyncio.Task"""
+
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Synchronous coroutine that takes in an integer argument that waits
+    for a random delay  and eventually returns it."""
+    list_sec = []
+    sorted_list = []
+
+    for _ in range(n):
+        res = task_wait_random(max_delay)
+        list_sec.append(res)
+    
+    for q in asyncio.as_completed(list_sec):
+        result = await q
+        sorted_list.append(result)
+
+    return sorted_list
