146b0165fa14a452517f56d9c7878c235c049fd3
david-develop <924@holbertonschool.com>
2021-07-21 23:00:13 -0500

    doc

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Coroutine tat execute async_comprehension four times in parallel using asyncio.gather"""
 
 
 from time import time
@@ -7,6 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Coroutine tat execute async_comprehension four times in parallel using asyncio.gather"""
     s = time()
     tasks = [async_comprehension() for _ in range(4)]
     await asyncio.gather(*tasks)
