56959f925f8b63e4ff56d967000a19af909595cb
david-develop <924@holbertonschool.com>
2021-07-29 19:13:16 -0500

    add simple cache system

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:
+      - Basic caching system inherit from BaseCaching
+    """
+    def __init__(self):
+        """Instantiation
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Assign items to cache data"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item value from key in dict"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
