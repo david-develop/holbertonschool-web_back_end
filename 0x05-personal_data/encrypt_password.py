cd4812b672a97417e71216ce69ca97b6d6e20ae5
david-develop <924@holbertonschool.com>
2021-08-01 17:50:10 -0500

    is_valid function

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -12,3 +12,11 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    is_valid function that takes a hashed password and validate
+    if it is valid
+    """
+    return  bcrypt.checkpw(password.encode(), hashed_password)
