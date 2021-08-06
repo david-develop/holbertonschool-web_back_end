e39a9e1082cd05687f9fe9e75da11b87ebaa8eaf
david-develop <924@holbertonschool.com>
2021-08-05 23:07:57 -0500

    user_object_from_credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ BasicAuth module
 """
-from typing import Tuple
+from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -42,3 +43,23 @@ class BasicAuth(Auth):
             return (None, None)
         data = decoded_base64_authorization_header.split(':')
         return (data[0], data[1])
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Return None if user_pwd is not the password of the User 
+        instance found"""
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
