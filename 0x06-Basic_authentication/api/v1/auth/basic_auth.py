7de7359499bebd1dea46ab4f06acca75c0dd7714
david-develop <924@holbertonschool.com>
2021-08-05 22:30:15 -0500

    decode_base64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ BasicAuth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,14 @@ class BasicAuth(Auth):
                 or authorization_header.split(' ')[0] != 'Basic':
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Return the decoded value as UTF8 string"""
+        if base64_authorization_header is None or\
+                type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode("utf8")
+        except:
+            return None
