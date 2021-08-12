ac29c7c6e84291ae28f2a357d777e23585142d36
david-develop <924@holbertonschool.com>
2021-08-11 20:23:30 -0500

    session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 """ SessionAuth module
 """
-from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
-import base64
-from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Method that creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
