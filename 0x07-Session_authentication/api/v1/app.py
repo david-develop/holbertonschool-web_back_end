4e654fa5ababcf25c6b28ca9bccaebc34b81ff63
david-develop <924@holbertonschool.com>
2021-08-11 20:12:21 -0500

    session auth class

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -4,11 +4,11 @@ Route module for the API
 """
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
@@ -21,6 +21,8 @@ if auth_type == 'auth':
     auth = Auth()
 elif auth_type == 'basic_auth':
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" SessionAuth module
+"""
+from typing import Tuple, TypeVar
+from api.v1.auth.auth import Auth
+import base64
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """SessionAuth class
+    """
+    pass
