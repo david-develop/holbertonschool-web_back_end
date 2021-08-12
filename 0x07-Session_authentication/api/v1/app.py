d118f351e0f191b291ab41198417d7722f2eefab
david-develop <924@holbertonschool.com>
2021-08-11 23:20:17 -0500

    session exp

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
 from api.v1.auth.session_auth import SessionAuth
@@ -23,6 +24,8 @@ elif auth_type == 'basic_auth':
     auth = BasicAuth()
 elif auth_type == 'session_auth':
     auth = SessionAuth()
+elif auth_type == 'session_exp_auth':
+    auth = SessionExpAuth()
 
 
 @app.before_request
