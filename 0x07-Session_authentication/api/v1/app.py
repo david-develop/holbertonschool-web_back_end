850cac73629d50452c5367a26507a0b6d0cc9a1b
david-develop <924@holbertonschool.com>
2021-08-11 21:29:23 -0500

    5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -30,10 +30,13 @@ def before_request_f():
     """ Before request auth handler
     """
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if auth is not None:
         if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request)\
+                    and not auth.session_cookie(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
