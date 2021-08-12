f8f0d2f4e5ba6bfeb049462c720da7175d9cdf11
david-develop <924@holbertonschool.com>
2021-08-11 22:47:28 -0500

    Flask view that handles all routes for the Session authentication

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Module of Session Auth views
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+    """ POST /api/v1/auth_session/login
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+      - 400 if can't return the User
+    """
+
+    req_par = request.form
+    error_msg = None
+    email = req_par.get('email')
+    password = req_par.get('password')
+
+    if email is None or email == '':
+        error_msg = 'email missing'
+        return jsonify({'error': error_msg}), 400
+    if password is None or password == '':
+        error_msg = 'password missing'
+        return jsonify({'error': error_msg}), 400
+
+    try:
+        found_users = User.search({'email': email})
+    except Exception:
+        error_msg = 'no user found for this email'
+        return jsonify({'error': error_msg}), 404
+
+    if not found_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    from api.v1.app import auth
+
+    user_valid = None
+    for user in found_users:
+        if user.is_valid_password(password):
+            user_valid = user
+            break
+
+    if user_valid is None:
+        error_msg = 'wrong password'
+        return jsonify({'error': error_msg}), 401
+
+    session_id = auth.create_session(user_valid.id)
+    session_name = os.getenv("SESSION_NAME")
+    res = jsonify(user_valid.to_json())
+    res.set_cookie(session_name, session_id)
+
+    return res
