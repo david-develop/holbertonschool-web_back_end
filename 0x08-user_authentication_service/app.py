07b6c9e9ceebae94d94d167311ec2a21bd6814bf
david-develop <924@holbertonschool.com>
2021-08-16 15:15:08 -0500

    d

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world() -> str:
-    """Basic route welcome"""
-    msj = {"message": "Bienvenue"}
-    return jsonify(msj)
+    """ Basic route GET"""
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
