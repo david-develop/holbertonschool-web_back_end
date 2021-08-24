adcf01dd14b899e06c95600be2752408b5d9bec4
david-develop <924@holbertonschool.com>
2021-08-23 21:42:50 -0500

    tc

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -5,14 +5,17 @@ Integration test module
 import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
+from unittest.mock import patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient class"""
+
     @parameterized.expand([
         ('google'),
         ('abc')
     ])
+    @patch('client.get_json')
     def test_org(self, input, mock):
         """Test mock org"""
         test_class = GithubOrgClient(input)
