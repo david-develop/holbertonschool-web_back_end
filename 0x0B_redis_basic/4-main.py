613417ecc4c4c68b25b9e4713a8ab593f8353d0d
david-develop <david.peraltar@ab-inbev.com>
2021-09-04 22:41:44 -0500

    web cache

--- 0x0B_redis_basic/4-main.py
+++ 0x0B_redis_basic/4-main.py
@@ -5,8 +5,8 @@ Cache = __import__('exercise').Cache
 replay = __import__('exercise').replay
 
 cache = Cache()
-# cache.store("foo")
-# cache.store("bar")
-# cache.store(42)
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
 
 replay(cache.store)
