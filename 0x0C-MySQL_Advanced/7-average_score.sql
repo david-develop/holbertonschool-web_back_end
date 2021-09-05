06651d840dcd8a534e9d81ecbc11987219532e74
david-develop <david.peraltar@ab-inbev.com>
2021-09-05 16:24:44 -0500

    avg

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,17 @@
+-- Script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$ 
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END
+$$
+DELIMITER ;
