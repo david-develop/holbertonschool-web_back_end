efdf5c8f9e39741b550422873e623dd8dbc0424c
david-develop <david.peraltar@ab-inbev.com>
2021-09-16 22:15:51 -0500

    function

--- 0x10-ES6_classes/2-main.js
+++ 0x10-ES6_classes/2-main.js
@@ -6,13 +6,14 @@ c1.name = 'Python 101';
 console.log(c1);
 
 try {
-  c1.name = 12;
+  c1.name = 'test';
 } catch (err) {
   console.log(err);
 }
 
 try {
-  const c2 = new HolbertonCourse('ES6', '1', ['Bob', 'Jane']);
+  const c2 = new HolbertonCourse('ES6', 1, ['Bob', 'Jane']);
+  console.log(c2)
 } catch (err) {
   console.log(err);
 }
---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    console.log(`${this._name} (${this._code})`);
+  }
+}
