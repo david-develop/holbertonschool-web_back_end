d2ef85ddaf75435abbea4f26b319510d2585e042
david-develop <david.peraltar@ab-inbev.com>
2021-09-16 22:36:14 -0500

    princing

--- 0x10-ES6_classes/3-main.js
+++ 0x10-ES6_classes/3-main.js
@@ -1,4 +1,6 @@
 import Currency from './3-currency';
 
 const dollar = new Currency('$', 'Dollars');
+console.log(typeof dollar);
+console.log(dollar instanceof Currency);
 console.log(dollar.displayFullCurrency());
