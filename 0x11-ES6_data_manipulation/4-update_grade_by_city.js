a677672127c7f1f97f20a9aba1d1a906c477fec1
david-develop <david.peraltar@ab-inbev.com>
2021-09-21 21:55:28 -0500

    f

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,18 +6,23 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     return [];
   }
 
-  return students.filter((student) => student.location === city)
-    .map((student) => {
-      const grades = newGrades.filter((grade) => grade.studentId === student.id);
-      let gradeadd;
-      if (grades[0]) {
-        gradeadd = grades[0].grade;
-      } else {
-        gradeadd = 'N/A';
-      }
-      return {
-        ...student,
-        gradeadd,
-      };
-    });
+  const stByCity = students.filter((student) => student.location === city);
+
+  const stWithGrade = stByCity.map((student) => {
+    const grades = newGrades.filter((grade) => grade.studentId === student.id);
+
+    let gradeadd;
+
+    if (grades[0]) {
+      gradeadd = grades[0].grade;
+    } else {
+      gradeadd = 'N/A';
+    }
+    return {
+      ...student,
+      gradeadd,
+    };
+  });
+
+  return stWithGrade;
 }
