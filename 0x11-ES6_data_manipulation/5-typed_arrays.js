c30fa5cbb73b8db36349c7514707e2cdbb5c0f6b
david-develop <david.peraltar@ab-inbev.com>
2021-09-23 20:10:14 -0500

    fas

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Uint8Array(buffer);
+  if (position > length - 1) {
+    throw Error('Position outside range');
+  }
   view[position] = value;
   const dataViewObj = new DataView(buffer);
   return dataViewObj;
