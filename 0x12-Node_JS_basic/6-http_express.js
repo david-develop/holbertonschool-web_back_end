d87c6c81a3a5adbf81d9d18437e2da61c3a228ff
david-develop <david.peraltar@ab-inbev.com>
2021-09-27 18:05:27 -0500

    message fix

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const app = express();
+const hostname = 'localhost';
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://${hostname}:${port}`);
 });
 
 module.exports = app;
