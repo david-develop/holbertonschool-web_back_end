c31aa52e527f66f860c0ff704f32817591f2d1bd
david-develop <david.peraltar@ab-inbev.com>
2021-09-26 23:25:29 -0500

    types

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,12 @@
 export default function cleanSet(set, startString) {
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+
   const finalArr = [];
   const point = startString.length;
   set.forEach((element) => {
