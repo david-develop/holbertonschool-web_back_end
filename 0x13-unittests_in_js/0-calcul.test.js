26f7c2c0a2568cf6c6e11cc8c51180382b46f68c
david-develop <david.peraltar@ab-inbev.com>
2021-09-29 18:47:39 -0500

    test lint

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,17 @@
-const assert = require('assert');
 const calculateNumber = require('./0-calcul');
+const assert = require('assert');
 
-describe('calculateNumber', () => {
-  it('Two integers', () => {
-    assert.deepStrictEqual(calculateNumber(1, 3), 4);
-  });
-  it('Integer and float', () => {
+describe("calculateNumber", function () {
+  it("should round a and b and return the sum", function () {
     assert.deepStrictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('Foalt and float', () => {
+  it("should round a and b and return the sum", function () {
     assert.deepStrictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('Foalt and float', () => {
+  it("should round a and b and return the sum", function () {
     assert.deepStrictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it('Foalt and integer', () => {
-    assert.deepStrictEqual(calculateNumber(3.7, 1), 5);
-  });
-});
+  it("should round a and b and return the sum", function () {
+    assert.deepStrictEqual(calculateNumber(1.4, 3.4), 4);
+  } )
+})
