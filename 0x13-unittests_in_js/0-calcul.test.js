9a094ab67d1609d58f8b1aa61e518210b825a9e3
david-develop <david.peraltar@ab-inbev.com>
2021-09-29 18:46:31 -0500

    deepstrict test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,18 +3,18 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('Two integers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.deepStrictEqual(calculateNumber(1, 3), 4);
   });
   it('Integer and float', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.deepStrictEqual(calculateNumber(1, 3.7), 5);
   });
   it('Foalt and float', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.deepStrictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('Foalt and float', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.deepStrictEqual(calculateNumber(1.5, 3.7), 6);
   });
   it('Foalt and integer', () => {
-    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.deepStrictEqual(calculateNumber(3.7, 1), 5);
   });
 });
