6b4fbafdf2972f23c1ef4fbc67db3726f161e6d0
david-develop <david.peraltar@ab-inbev.com>
2021-09-29 18:50:52 -0500

    test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,20 @@
 const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
-describe("calculateNumber", function () {
-  it("should round a and b and return the sum", function () {
-    assert.deepStrictEqual(calculateNumber(1, 3.7), 5);
+describe('calculateNumber', () => {
+  it('should round a and b and return the sum', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it("should round a and b and return the sum", function () {
-    assert.deepStrictEqual(calculateNumber(1.2, 3.7), 5);
+  it('should round a and b and return the sum', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it("should round a and b and return the sum", function () {
-    assert.deepStrictEqual(calculateNumber(1.5, 3.7), 6);
+  it('should round a and b and return the sum', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it("should round a and b and return the sum", function () {
-    assert.deepStrictEqual(calculateNumber(1.4, 3.4), 4);
-  } )
-})
+  it('should round a and b and return the sum', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should round a and b and return the sum', () => {
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+  });
+});
