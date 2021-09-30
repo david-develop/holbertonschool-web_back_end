e64fcc7681d513fa18e87c5a0e27e8d8d9fea41e
david-develop <david.peraltar@ab-inbev.com>
2021-09-29 20:32:48 -0500

    afterEach beforeEach hooks sinon

---
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require("sinon");
+const expect = require('chai').expect
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
+
+  it('check sendPaymentRequestToApi outputs', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith(`The total is: 120`)).to.be.true;
+  });
+  it('check sendPaymentRequestToApi outputs', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledWith(`The total is: 20`)).to.be.true;
+  });
+});
