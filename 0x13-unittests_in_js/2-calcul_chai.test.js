a646df97c457ddc945785a896117138ae74417c1
david-develop <david.peraltar@ab-inbev.com>
2021-09-29 19:06:30 -0500

    impruves

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,29 +1,29 @@
-const expect = require('chai').expect
 const calculateNumber = require('./2-calcul_chai');
+const expect = require('chai').expect
 
 describe('calculateNumber', () => {
-  it('SUM Two integers', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('SUM', 1, 3)).to.equal(4);
   });
-  it('SUM Integer and float', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
   });
-  it('SUM Foalt and float', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
   });
-  it('SUM Foalt and float', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
   });
-  it('SUM Foalt and integer', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('SUM', 3.7, 1)).to.equal(5);
   });
-  it('SUBTRACT Foalt and float', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
-  it('DIVIDE Foalt and float', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
   });
-  it('DIVIDE Foalt and float', () => {
+  it('should round a and b and return by the type', () => {
     expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 });
