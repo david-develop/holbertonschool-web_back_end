adf8cbea93bb6eaf4d1ca98e40a65dafe0040fc3
david-develop <david.peraltar@ab-inbev.com>
2021-10-03 12:31:34 -0500

    redis connected

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+  host: '127.0.0.1',
+  port: '6379'
+});
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
